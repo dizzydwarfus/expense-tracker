e4b3abf2220abbad842eb6e646c8dd9a96e3b3c9
Lian Zhen Yang <dizzydwarfus@Lians-MacBook-Pro.local>
2024-01-28 16:17:45 +0100

    added models for user and transactions with class methods to insert/upsert each in ExpenseWebAppDB class, inheriting from MongoConnector

---
+++ mongoapp/__init__.py
@@ -0,0 +1,3 @@
+# from .utils.mongodb_connector import MongoConnector
+# from .utils._constants import categories_dict
+# from .utils._logger import MyLogger
---
+++ mongoapp/utils/__init__.py
@@ -0,0 +1,3 @@
+# from ._constants import categories_dict
+# from ._logger import MyLogger
+# from .mongodb_connector import MongoConnector
